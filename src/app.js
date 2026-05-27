544906ce56c96036a412be173a484297354dd722
Sanket01-debug <sanketkansal2001@gmail.com>
2026-05-27 22:13:25 +0530

    refactor: reorganize user route handlers for clarity and consistency

--- src/app.js
+++ src/app.js
@@ -2,21 +2,24 @@ const express = require ("express");
 
 const app = express();
 
-app.use("/hello/2", (req, res) => {
-    res.send("Abracadabra!");
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Sanket", lastName: "Kansal" });
 });
 
-app.use("/hello", (req, res) => {
-    res.send("Hello hello hello!");
+app.post("/user", (req, res) => {
+    // saving data to DB
+    res.send("Data successfully saved to the database!")
 });
 
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully!");
+})
+
+// this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
     res.send("Hello from the server!");
 });
-
-// app.use("/", (req, res) => {
-//     res.send("Namaste Sanket");
-// });
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777...");
