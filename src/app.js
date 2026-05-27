785e7f6f6cd3e53e62da176e3b302a413bc3fcdb
Sanket01-debug <sanketkansal2001@gmail.com>
2026-05-27 23:05:57 +0530

    refactor: update user route to handle parameters and remove unused routes

--- src/app.js
+++ src/app.js
@@ -3,22 +3,9 @@ const express = require ("express");
 const app = express();
 
 // This will only handle GET call to /user
-app.get("/user", (req, res) => {
+app.get("/user/:userID/:name/:password", (req, res) => {
+    console.log(req.params)
     res.send({ firstName: "Sanket", lastName: "Kansal" });
-});
-
-app.post("/user", (req, res) => {
-    // saving data to DB
-    res.send("Data successfully saved to the database!")
-});
-
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully!");
-})
-
-// this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-    res.send("Hello from the server!");
 });
 
 app.listen(7777, () => {
