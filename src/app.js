b0249b23e393218ec43e3f27588e52b9cb36f496
Sanket01-debug <sanketkansal2001@gmail.com>
2026-05-29 11:29:18 +0530

    refactor: enhance admin route middleware for authorization checks

--- src/app.js
+++ src/app.js
@@ -2,26 +2,37 @@ const express = require("express");
 
 const app = express();
 
-// GET /users => middleware chain => request handlet
-
-app.use("/", (req, res, next) => {
-    // res.send("Handling /route");   
-    next(); 
+//Handle Auth Middleware for all GET, POST, ... requests
+app.use("/admin", (req, res, next) => {
+    const token = "xyza";
+    const isAdminAuthorized = token === "xyz";
+    if(!isAdminAuthorized){
+        res.status(401).send("Unauthorized request")
+    }else{
+        next();
+    }
 });
 
-app.get(
-    "/user",
-    (req, res, next) => {
-     console.log("Handling /user route");
-        next();
-    },
-    (req, res, next) => {
-        next();
-    },
-    (req, res, next) => {
-        res.send("2nd Route Handler");
+app.get("/admin/getAllData", (req, res) => {
+    // Logic of checking if the request is authorized
+    const token = "xyza";
+    const isAdminAuthorized = token === "xyz";
+    if(isAdminAuthorized){
+        res.send("All Data Sent");
+    }else{
+        res.status(401).send("Unauthorized request");
     }
-);
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+    // Logic of checking if the request is authorized
+    const token = "xyza";
+    const isAdminAuthorized = token === "xyz";
+    if(isAdminAuthorized){
+        res.send("Deleted a user");
+    }else{
+        res.status(401).send("Unauthorized request");
+    }});
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777...");
