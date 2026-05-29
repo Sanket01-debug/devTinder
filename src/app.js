358785875a9b44b1abff67d59c8e6be75b039af6
Sanket01-debug <sanketkansal2001@gmail.com>
2026-05-29 10:35:58 +0530

    refactor: simplify user route middleware

--- src/app.js
+++ src/app.js
@@ -2,33 +2,24 @@ const express = require("express");
 
 const app = express();
 
-//app.use("/routes", rh, rH2,[rh3, rH4], rH5);
+// GET /users => middleware chain => request handlet
 
-app.use("/user", [(req, res, next) => {
-    // Route Handler
-    // res.send("Route Handler 1");
-    console.log("Handling the route user!!");
-    // res.send("Response!!"); 
-    next();
-},
-(req, res, next) => {
-    console.log("Handling the route user 2!!");
-    // res.send("2nd Response!!");
-    next();
-},
-(req, res, next) => {
-    console.log("Handling the route user 3!!");
-    // res.send("3rd Response!!");
-    next();
-}, ],
+app.use("/", (req, res, next) => {
+    // res.send("Handling /route");   
+    next(); 
+});
+
+app.get(
+    "/user",
     (req, res, next) => {
-        console.log("Handling the route user 4!!");
-        // res.send("4th Response!!");
+     console.log("Handling /user route");
         next();
     },
     (req, res, next) => {
-        console.log("Handling the route user 5!!");
-        res.send("5th Response!!");
+        next();
+    },
+    (req, res, next) => {
+        res.send("2nd Route Handler");
     }
 );
 
