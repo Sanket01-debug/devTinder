3cb647748fb6803ba94dfcbf5f606c9d97e86270
Sanket01-debug <sanketkansal2001@gmail.com>
2026-06-07 13:35:22 +0530

    feat: implement one user retrieval by email

--- src/app.js
+++ src/app.js
@@ -22,14 +22,26 @@ app.post("/signup", async (req, res) => {
 
 });
 
-app.get("/signup", async (req, res) => {
+// Get user by email 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
   try {
-    const users = await User.find({});
-    res.send(users);
+    const users = await User.find({emailId: userEmail}); 
+    if(users.length === 0){
+      res.status(404).send("User not found");
+    }else{
+      res.send(users);
+    }
   } catch (err) {
-    res.status(400).send("Error fetching users:"+err.message);
+    res.status(400).send("Something went wrong");
   }
 
+})
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  
 });
 
 
