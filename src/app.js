62ff2b91097817a1842c54d7266385eb2299a217
Sanket01-debug <sanketkansal2001@gmail.com>
2026-06-15 23:18:58 +0530

    feat: add validation for skills length in user update

--- src/app.js
+++ src/app.js
@@ -90,6 +90,9 @@ app.patch("/user/:userId", async(req, res) => {
     if(!isUpdateAllowed){
       throw new Error("Update not allowed");
     }
+    if(data?.skills.length > 10){
+      throw new Error("Skills cannot be more than 10");
+    }
     const user = await User.findByIdAndUpdate({ _id: userId}, data, {
       returnDocument:"after",
       runValidators:"true",
