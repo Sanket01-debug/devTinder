e5e230b49d71e1364028483dcef4f02dc37a2b76
Sanket01-debug <sanketkansal2001@gmail.com>
2026-06-17 17:14:57 +0530

    refactor: improve code formatting and structure in app.js

--- src/app.js
+++ src/app.js
@@ -10,17 +10,19 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   try {
-  // Validation of data
-  validateSignUpData(req);
+    // Validation of data
+    validateSignUpData(req);
 
-  const {password} = req.body;
+    const { firstName, lastName, emailId, password } = req.body;
 
-  // Encrypt the password
-  const passwordHash = await bcrypt.hash(password, 10);
-  console.log(passwordHash);
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
 
-  // Creating a new instance of the User model
-  const user = new User(req.body);
+    // Creating a new instance of the User model
+    const user = new User({
+      firstName, lastName, emailId, password: passwordHash,
+    });
 
     await user.save();
     res.send("User added successfully");
@@ -35,10 +37,10 @@ app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
 
   try {
-    const user = await User.findOne({emailId: userEmail}); 
-    if(!user){
-        res.status(404).send("User not found");
-    }else{
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
       res.send(user);
     }
 
@@ -58,10 +60,10 @@ app.get("/user", async (req, res) => {
 app.get("/feed", async (req, res) => {
 
   try {
-    const users = await User.find({}); 
-    if(users.length === 0){
+    const users = await User.find({});
+    if (users.length === 0) {
       res.status(404).send("User not found");
-    }else{
+    } else {
       res.send(users);
     }
   } catch (err) {
@@ -71,10 +73,10 @@ app.get("/feed", async (req, res) => {
 });
 
 // Delete a user from the database
-app.delete("/user", async(req, res) => {
+app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
   try {
-    const user = await User.findByIdAndDelete({_id: userId}); 
+    const user = await User.findByIdAndDelete({ _id: userId });
     // const user = await User.findByIdAndDelete(userId); 
 
     res.send("User deleted successfully");
@@ -85,25 +87,25 @@ app.delete("/user", async(req, res) => {
 });
 
 // Update data of the user
-app.patch("/user/:userId", async(req, res) => {
+app.patch("/user/:userId", async (req, res) => {
   const userId = req.params?.userId;
   const data = req.body;
 
-  
+
   try {
     const ALLOWED_UPDATES = [
       "photoUrl", "about", "gender", "age", "skills"
     ]
-    const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k)); 
-    if(!isUpdateAllowed){
+    const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k));
+    if (!isUpdateAllowed) {
       throw new Error("Update not allowed");
     }
-    if(data?.skills.length > 10){
+    if (data?.skills.length > 10) {
       throw new Error("Skills cannot be more than 10");
     }
-    const user = await User.findByIdAndUpdate({ _id: userId}, data, {
-      returnDocument:"after",
-      runValidators:"true",
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: "true",
     });
     // console.log(user);
     res.send("User updated successfully");
@@ -112,7 +114,7 @@ app.patch("/user/:userId", async(req, res) => {
   }
 
 })
- 
+
 
 connectDB()
   .then(() => {
