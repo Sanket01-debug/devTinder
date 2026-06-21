4a3c093679f8934c3153ccaac1636e39379505dc
Sanket01-debug <sanketkansal2001@gmail.com>
2026-06-21 15:52:33 +0530

    refactor: remove unused user routes and clean up code

--- src/app.js
+++ src/app.js
@@ -77,91 +77,18 @@ app.get("/profile", userAuth, async (req, res) => {
     res.status(400).send("ERROR : " + err.message);
   }
 
-})
-
-// Get user by email 
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.emailId;
-
-  try {
-    const user = await User.findOne({ emailId: userEmail });
-    if (!user) {
-      res.status(404).send("User not found");
-    } else {
-      res.send(user);
-    }
-
-    // const users = await User.find({emailId: userEmail}); 
-    // if(users.length === 0){
-    //   res.status(404).send("User not found");
-    // }else{
-    //   res.send(users);
-    // }
-  } catch (err) {
-    res.status(400).send("Something went wrong");
-  }
-
-})
-
-// Feed API - GET /feed - get all the users from the database
-app.get("/feed", async (req, res) => {
-
-  try {
-    const users = await User.find({});
-    if (users.length === 0) {
-      res.status(404).send("User not found");
-    } else {
-      res.send(users);
-    }
-  } catch (err) {
-    res.status(400).send("Something went wrong");
-  }
-
 });
 
-// Delete a user from the database
-app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-  try {
-    const user = await User.findByIdAndDelete({ _id: userId });
-    // const user = await User.findByIdAndDelete(userId); 
+app.post("/sendConnectionRequest", userAuth, async(req, res) => {
 
-    res.send("User deleted successfully");
+  const user = req.user;
 
-  } catch (err) {
-    res.status(400).send("Something went wrong");
-  }
-});
+  // Sending a connection request
+  console.log("Sending a connection request");
 
-// Update data of the user
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-
-  try {
-    const ALLOWED_UPDATES = [
-      "photoUrl", "about", "gender", "age", "skills"
-    ]
-    const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k));
-    if (!isUpdateAllowed) {
-      throw new Error("Update not allowed");
-    }
-    if (data?.skills.length > 10) {
-      throw new Error("Skills cannot be more than 10");
-    }
-    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnDocument: "after",
-      runValidators: "true",
-    });
-    // console.log(user);
-    res.send("User updated successfully");
-  } catch (err) {
-    res.status(400).send("UPDATE FAILED:" + err.message);
-  }
+  res.send(user.firstName + " sent the connection request");
 
 })
-
 
 connectDB()
   .then(() => {
