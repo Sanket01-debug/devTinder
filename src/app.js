ae281b1cb3d75b9d6ec4e7923860ca2ad932fbdf
Sanket01-debug <sanketkansal2001@gmail.com>
2026-06-21 15:16:11 +0530

    feat: implement user authentication middleware for profile access

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth")
 
 app.use(express.json());
 app.use(cookieParser());
@@ -51,7 +52,7 @@ app.post("/login", async (req, res) => {
 
       // Create a JWT Token
 
-      const token = await jwt.sign({ _id: user._id }, "DEV@TINDER$790")
+      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790")
 
       // Add the token to cookie and send the response back to the user
       res.cookie("token", token);
@@ -66,25 +67,11 @@ app.post("/login", async (req, res) => {
 
 })
 
-app.get("/profile", async (req, res) => {
+app.get("/profile", userAuth, async (req, res) => {
 
   try {
-    const cookies = req.cookies;
 
-    const { token } = cookies;
-    if (!token) {
-      throw new Error("Invalid Token");
-    }
-
-    const decodedMessage = await jwt.verify(token, "DEV@TINDER$790")
-
-    const { _id } = decodedMessage;
-
-    const user = await User.findById(_id);
-    if(!user){
-      throw new Error("User does not exist");
-    }
-
+    const user = req.user;
     res.send(user);
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
