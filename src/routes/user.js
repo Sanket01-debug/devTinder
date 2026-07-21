eeb3b99070ce6ae061249abf0f52b8b17aa34ec4
Sanket01-debug <sanketkansal2001@gmail.com>
2026-07-21 12:33:08 +0530

    feat: add user requests retrieval endpoint for logged-in users

---
+++ src/routes/user.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const userRouter = express.Router();
+
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest")
+
+// Get all the pending connection request for the loggedIn user
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            toUserId: loggedInUser._id,
+            status: "interested",
+        });
+
+        res.json({message: "Data fetched successfully", data: connectionRequests});
+
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+module.exports = userRouter;
