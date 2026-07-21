c0754be05444c6e9e851e1c3902c5eb5ff7324b4
Sanket01-debug <sanketkansal2001@gmail.com>
2026-07-21 19:12:01 +0530

    feat: refactor user data population to use constant for safe data fields

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,9 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest")
 
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills"
+
+
 // Get all the pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     try {
@@ -14,7 +17,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
             status: "interested",
         }).populate(
             "fromUserId",
-            "firstName lastName photoUrl age gender about skills"
+            USER_SAFE_DATA
         );
         // }).populate("fromUserId", ["firstName", "lastName"]);
 
@@ -22,6 +25,25 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
 
     } catch (err) {
         res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+userRouter.get("/user/connections", userAuth, async(req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
+            ],
+        }).populate("fromUserId", USER_SAFE_DATA);
+
+        const data = connectionRequests.map((row) => row.fromUserId);
+
+        res.json({ data });
+    } catch (err) {
+        res.status(400).send({ message: err.message });
     }
 });
 
