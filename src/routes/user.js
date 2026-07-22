30b736fb121b43ae2f16d33434ba6bdbe33e8fd1
Sanket01-debug <sanketkansal2001@gmail.com>
2026-07-22 19:14:24 +0530

    feat: implement pagination for feed endpoint with skip and limit

--- src/routes/user.js
+++ src/routes/user.js
@@ -61,6 +61,11 @@ userRouter.get("/feed", userAuth, async (req, res) => {
 
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.page) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
         const connectionRequests = await ConnectionRequest.find({
             $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
         }).select("fromUserId toUserId");
@@ -72,13 +77,13 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         });
 
         const users = await User.find({
-           $and: [ 
-            {_id: { $nin: Array.from(hideUsersFromFeed)}},
-            {_id: {$ne: loggedInUser}},
-           ],
-        }).select(USER_SAFE_DATA);
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser } },
+            ],
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
 
-        res.send(users);
+        res.json({ data: users });
 
     } catch (err) {
         res.status(400).json({ message: err.message });
