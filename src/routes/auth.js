7dc9861a795a9619a4f06e19d9db347abe625f1f
Sanket01-debug <sanketkansal2001@gmail.com>
2026-08-26 11:55:24 +0530

    feat: integrate email sending functionality using Resend service

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const sendEmail = require("../utils/sendEmail");
 const authRouter = express.Router();
 
 const { validateSignUpData } = require("../utils/validation");
@@ -16,7 +17,7 @@ authRouter.post("/signup", async (req, res) => {
     const passwordHash = await bcrypt.hash(password, 10);
     console.log(passwordHash);
 
-    //   Creating a new instance of the User model
+    // Creating a new instance of the User model
     const user = new User({
       firstName,
       lastName,
@@ -24,15 +25,38 @@ authRouter.post("/signup", async (req, res) => {
       password: passwordHash,
     });
 
+    // Save user to database
     const savedUser = await user.save();
+
+    console.log("USER SAVED:", savedUser.emailId);
+
+    // Send welcome email
+    await sendEmail({
+      to: emailId,
+      subject: "Welcome to DevTinder!",
+      html: `
+        <h2>Welcome to DevTinder, ${firstName}!</h2>
+        <p>Your account has been successfully created.</p>
+        <p>We are happy to have you on DevTinder.</p>
+      `,
+    });
+
+    console.log("EMAIL FUNCTION COMPLETED");
+
+    // Generate JWT token
     const token = await savedUser.getJWT();
 
+    // Store token in cookie
     res.cookie("token", token, {
       expires: new Date(Date.now() + 8 * 3600000),
     });
 
-    res.json({ message: "User Added successfully!", data: savedUser });
+    res.json({
+      message: "User Added successfully!",
+      data: savedUser,
+    });
   } catch (err) {
+    console.error("Signup Error:", err);
     res.status(400).send("ERROR : " + err.message);
   }
 });
@@ -42,9 +66,11 @@ authRouter.post("/login", async (req, res) => {
     const { emailId, password } = req.body;
 
     const user = await User.findOne({ emailId: emailId });
+
     if (!user) {
       throw new Error("Invalid credentials");
     }
+
     const isPasswordValid = await user.validatePassword(password);
 
     if (isPasswordValid) {
@@ -53,6 +79,7 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
       });
+
       res.send(user);
     } else {
       throw new Error("Invalid credentials");
@@ -66,6 +93,7 @@ authRouter.post("/logout", async (req, res) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
   });
+
   res.send("Logout Successful!!");
 });
 
---
+++ src/utils/sendEmail.js
@@ -0,0 +1,28 @@
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const sendEmail = async ({ to, subject, html }) => {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: process.env.EMAIL_FROM,
+      to: [to],
+      subject,
+      html,
+    });
+
+    if (error) {
+      console.error("Resend error:", error);
+      throw new Error(error.message);
+    }
+
+    console.log("Email sent successfully:", data.id);
+
+    return data;
+  } catch (error) {
+    console.error("Email sending failed:", error);
+    throw error;
+  }
+};
+
+module.exports = sendEmail;
